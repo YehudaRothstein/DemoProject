6a8001d055025063481ea3998520721c394fc8af
ExcaliburFRC <excaliburfrc@gmail.com>
2025-08-31 17:55:26 +0300

    add printHello method

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+
+
+    public static void printHello(){
+        System.out.println("hello");
+    }
 }
